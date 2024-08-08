2fe0a7ed956ff6ade6b4c071bc5b216a9cecd5e9
Laksa Bayu Trie Aji <laksabayu18@gmail.com>
2024-08-08 23:11:43 +0700

    Add comment

--- src/Person.java
+++ src/Person.java
@@ -12,6 +12,7 @@ class Person {
 
     //Constructor overloading -> constructor yang bisa dibuat lebih dari satu, dengan syarat tipe data parameter harus berbeda, atau jumlah parameter harus berbeda
     Person(String paramName){
+        // name = paramName -> Tanpa menggunakan this
         this(paramName, null);
     }
 
