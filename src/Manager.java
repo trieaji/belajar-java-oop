342ce9a748353eedea257ca50c16b474e78f064d
Laksa Bayu Trie Aji <laksabayu18@gmail.com>
2024-08-14 23:26:42 +0700

    Add comment inheritance and super constructor

--- src/Manager.java
+++ src/Manager.java
@@ -5,6 +5,17 @@ class Manager {
     void sayHello(String name) {
         System.out.println("Hi " + name + ", My name is " + this.name);
     }
+
+    //Saat membuat super constructor di parent(class), maka turunannya hrus dibuatkan constructorny juga (super constructor)
+    Manager(String name) {
+        this.name = name;
+    }
+
+    /* Default constructor -> Maka turunannya tidak wajib dibuatkn constructornya (super constructor)
+    Manager(string name) {
+        this.name = name;
+    }
+    */
 }
 
 // Bisa membuat inheritance di dalam satu file atau di file terpisah
